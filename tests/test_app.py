921b49a0278ca46e14e2ff4eb853e43f8391d560
Sunrise443 <sunkensun0@gmail.com>
2025-03-28 14:12:03 +0300

    async tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,84 +1,103 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import ASGITransport, AsyncClient
 
 from app.api import app
 
-client = TestClient(app)
+
 
 #For plans
-def test_add_plan():
+@pytest.mark.asyncio
+async def test_add_plan():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "plan_name": "Закончить проект",
     "date": "2025-03-04",
     "completed": False
   }
-  response = client.post("/planner/plans", json=test_data)
+  async with client:
+    response = await client.post("/planner/plans", json=test_data)
   assert response.status_code == 200
   assert "plan_id" in response.json() #проверяет наличие ключа в ответе от сервера
 
-def test_all_plan():
+@pytest.mark.asyncio
+async def test_all_plan():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "plan_name": "Закончить проект",
     "date": "2025-03-04",
     "completed": False
   }
-  post_response = client.post("/planner/plans", json=test_data)
-  assert post_response.status_code == 200
-  plan_id = post_response.json()["plan_id"]
+  async with client:
+    post_response = await client.post("/planner/plans", json=test_data)
+    plan_id = post_response.json()["plan_id"]
+    response = await client.get("/planner/plans")
 
-  response = client.get("/planner/plans")
+  assert post_response.status_code == 200
   assert response.status_code == 200
   assert {**test_data, "id": plan_id} in response.json()
 
 #For goals
-def test_add_goal():
+@pytest.mark.asyncio
+async def test_add_goal():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "goal_name": "Закончить проект",
     "date": "2025-03-04",
     "completed": False
   }
-  response = client.post("/planner/goals", json=test_data)
+  async with client:
+    response = await client.post("/planner/goals", json=test_data)
   assert response.status_code == 200
   assert "goal_id" in response.json()
 
-def test_all_goal():
+@pytest.mark.asyncio
+async def test_all_goal():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "goal_name": "Закончить проект",
     "date": "2025-03-04",
     "completed": False
   }
-  post_response = client.post("/planner/goals", json=test_data)
+  async with client:
+    post_response = await client.post("/planner/goals", json=test_data)
+    goal_id = post_response.json()["goal_id"]
+    response = await client.get("/planner/goals")
+  
   assert post_response.status_code == 200
-  goal_id = post_response.json()["goal_id"]
-
-  response = client.get("/planner/goals")
   assert response.status_code == 200
   assert {**test_data, "id": goal_id} in response.json()
 
 #For notes
-def test_add_note():
+@pytest.mark.asyncio
+async def test_add_note():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "note_name": "Закончить проект",
     "date": "2025-03-04"
   }
-  response = client.post("/planner/notes", json=test_data)
+  async with client:
+    response = await client.post("/planner/notes", json=test_data)
   assert response.status_code == 200
   assert "note_id" in response.json() #проверяет наличие ключа в ответе от сервера
 
-def test_all_note():
+@pytest.mark.asyncio
+async def test_all_note():
+  client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
   test_data = {
     "user_id": 1,
     "note_name": "Закончить проект",
     "date": "2025-03-04"
   }
-  post_response = client.post("/planner/notes", json=test_data)
-  assert post_response.status_code == 200
-  note_id = post_response.json()["note_id"]
+  async with client:
+    post_response = await client.post("/planner/notes", json=test_data)
+    note_id = post_response.json()["note_id"]
+    response = await client.get("/planner/notes")
 
-  response = client.get("/planner/notes")
+  assert post_response.status_code == 200
   assert response.status_code == 200
   assert {**test_data, "id": note_id} in response.json()
